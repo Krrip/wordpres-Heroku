0eaf08e9f029e6b66e65f52c65d4a5fcd3ead5d3
Krrip <52692483+Krrip@users.noreply.github.com>
2021-08-26 09:48:53 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Download and install xRay
-mkdir /tmp/wordpress
-curl -L -H "Cache-Control: no-cache" -o /tmp/wordpress/wordpress.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-windows-64.zip
-unzip /tmp/wordpress/wordpress.zip -d /tmp/wordpress
-install -m 755 /tmp/wordpress/wordpress /usr/local/bin/wordpress
+mkdir /tmp/xray
+curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
+unzip /tmp/xray/xray.zip -d /tmp/xray
+install -m 755 /tmp/xray/xray /usr/local/bin/xray
 
 # Remove temporary directory
-rm -rf /tmp/wordpress
+rm -rf /tmp/xray
 
 # XRay new configuration
-install -d /usr/local/etc/wordpress
-cat << EOF > /usr/local/etc/wordpress/config.json
+install -d /usr/local/etc/xray
+cat << EOF > /usr/local/etc/xray/config.json
 {
     "inbounds": [
         {
@@ -40,4 +40,4 @@ cat << EOF > /usr/local/etc/wordpress/config.json
 EOF
 
 # Run XRay
-/usr/local/bin/wordpress -config /usr/local/etc/wordpress/config.json
+/usr/local/bin/xray -config /usr/local/etc/xray/config.json
